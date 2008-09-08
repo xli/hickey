8366f2bbf8792fbc77cf76f3ffa61cc260fb3afd
Li Xiao <iam@li-xiao.com>
2008-09-08 09:54:24 +0800

    clean code

--- lib/hickey/acceptor.rb
+++ lib/hickey/acceptor.rb
@@ -10,14 +10,8 @@ module Hickey
         visitor.visit_array(klass, self)
       end
     end
-    module Object
-      def accept_for_hickey(klass, visitor)
-        self
-      end
-    end
   end
 end
 
-Object.send(:include, Hickey::Acceptor::Object)
 Array.send(:include, Hickey::Acceptor::Array)
 Hash.send(:include, Hickey::Acceptor::Hash)
