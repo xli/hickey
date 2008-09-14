01a9c3e201a9276d019407eea88e0effbcecd82a
Li Xiao <iam@li-xiao.com>
2008-09-14 13:47:02 +0800

    updated todo; added should_bypass_callbacks_and_validations for property_definition models

--- test/models/property_definition.rb
+++ test/models/property_definition.rb
@@ -1,14 +1,18 @@
 class PropertyDefinition < ActiveRecord::Base
   belongs_to :project
+  should_bypass_all_callbacks_and_validations
 end
 
 class EnumPropertyDefinition < PropertyDefinition
   has_many :enum_values, :foreign_key => 'property_definition_id'
+  should_bypass_all_callbacks_and_validations
 end
 
 class UserPropertyDefinition < PropertyDefinition
+  should_bypass_all_callbacks_and_validations
 end
 
 class EnumValue < ActiveRecord::Base
   belongs_to :property_definition, :class_name => "EnumeratedPropertyDefinition", :foreign_key => "property_definition_id"
+  should_bypass_all_callbacks_and_validations
 end
